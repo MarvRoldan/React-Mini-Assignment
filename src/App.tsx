9e1fa35a444d19736cddf35b108c0b0f01fd29a2
MarvRoldan <marvinroldan_@hotmail.com>
2020-04-15 00:57:41 -0600

    Added button and message

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    message: 'Hello'
+  }
+
+  /*changeMessage = {newMessage} => {
+    this.setState({
+      message:newMessage
+    });
+  }*/
+
+  render() {
+    return (
+      <Fragment>
+        <button>CLICK!</button>
+        <div>{this.state.message}</div>
+      </Fragment>
+    );
+  }
 }
-
-export default App;
